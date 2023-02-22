3fbb1b3b4aa97a5ba13ca2435d63be6a47251970
LeHoaiNhan <32263459+LeHoaiNhan@users.noreply.github.com>
2023-02-22 16:41:54 +0700

    fpt
    fpt

---
+++ AspNetCoreAPI/AspNetCoreAPI/Models/User/UserConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreAPI.Models.User
+{
+    public class UserConstants
+    {
+
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel() { Username = "a", EmailAddress = "jason.admin@email.com", Password = "a", GivenName = "Jason", Surname = "Bryant", Role = "Administrator" },
+            new UserModel() { Username = "b", EmailAddress = "elyse.seller@email.com", Password = "b", GivenName = "Elyse", Surname = "Lambert", Role = "Seller" },
+        };
+    }
+}
---
+++ AspNetCoreAPI/AspNetCoreAPI/Models/User/UserLogin.cs
@@ -0,0 +1,9 @@
+
+namespace AspNetCoreAPI.Models.User
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
